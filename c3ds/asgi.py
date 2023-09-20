100e3151b796ec25e71e949cd3944e2cb101b488
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-09-20 13:26:07 +0000

    Deployment commit

---
+++ c3ds/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for c3ds project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'c3ds.settings')
+
+application = get_asgi_application()
---
+++ env.py
@@ -0,0 +1,12 @@
+import os
+
+os.environ[
+    'DATABASE_URL'
+] = 'postgres://cozboapq:bSMg9JgbgOmWP7zcw-0dYTaXRxx1EFy1@trumpet.db.elephantsql.com/cozboapq'
+os.environ[
+    'SECRET_KEY'
+] = '53cr3tk3yc3ds'
+os.environ[
+    'CLOUDINARY_URL'
+] = 'cloudinary://545166596317496:NJ3i-qR_QOdTXgsbUfnDvcBPLAY@ddqyimddv'
+os.environ['DEBUG'] = '1'
